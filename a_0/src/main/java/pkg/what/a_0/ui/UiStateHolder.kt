60c55ec180da6e82fbfa7eb58d7c39a69cd6081f
P0Q0 <100450065+P0Q0@users.noreply.github.com>
2022-03-08 09:21:28 -0800

    updated uistateholder definition

--- a_0/src/main/java/pkg/what/a_0/ui/UiStateHolder.kt
+++ a_0/src/main/java/pkg/what/a_0/ui/UiStateHolder.kt
@@ -1,8 +1,22 @@
 package pkg.what.a_0.ui
 
+import androidx.lifecycle.ViewModel
+import pkg.what.a_0.domain.controller.ViewModelDisplay
+import pkg.what.a_0.domain.controller.ViewModelLogin
 import pkg.what.a_0.domain.controller.ViewModelPQ
 
-/** @desc  TODO:UiStateHolder */
-class UiStateHolder {
-    val vm: ViewModelPQ by lazy { ViewModelPQ() }
+/** @desc holds instance of view model that will expose data to the ui
+ * , updates state managed through the callers lifecycle */
+sealed class UiStateHolder {
+    val vmPQ: ViewModelPQ by lazy { ViewModelPQ() }
+    val vmLogin: ViewModelLogin by lazy { ViewModelLogin() }
+    val vmDisplay: ViewModelDisplay by lazy { ViewModelDisplay() }
+    data class ALIVE(val status: String) : UiStateHolder()
+    data class DEAD(val status: String) : UiStateHolder()
+    data class UNKNOWN(val status: String) : UiStateHolder()
+    object UiShState : UiStateHolder() {
+        fun update(type : Class<ViewModel>) {
+            /** @TODO: UiStateHolder: update state here */
+        }
+    }
 }
