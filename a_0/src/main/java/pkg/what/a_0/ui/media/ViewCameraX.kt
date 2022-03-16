fa7aa8825bc26e03c6069d409515ddfe3445dcd9
P0Q0 <100450065+P0Q0@users.noreply.github.com>
2022-03-15 19:05:38 -0700

    resolved dependency issues for camerax, it's valiated and verified working healthy application

--- a_0/src/main/java/pkg/what/a_0/ui/media/ViewCameraX.kt
+++ a_0/src/main/java/pkg/what/a_0/ui/media/ViewCameraX.kt
@@ -1,30 +1,45 @@
 package pkg.what.a_0.ui.media
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.Quality
+import androidx.camera.video.MediaStoreOutputOptions
+import androidx.camera.video.QualitySelector
 import androidx.camera.video.Recorder
 import androidx.camera.video.Recording
 import androidx.camera.video.VideoCapture
-import androidx.core.app.ActivityCompat
+import androidx.camera.video.VideoRecordEvent
 import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import pkg.what.a_0.R
 import pkg.what.a_0.domain.core.constant.FragLcTags
 import pkg.what.pq.databinding.CameraxLayoutBinding
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.Locale
 
 typealias LumaListener = (luma: Double) -> Unit
 
@@ -34,11 +49,11 @@ class ViewCameraX : Fragment() {
 
     private var navCntrl: NavController? = null
 
-    private var imgCapture: ImageCapture? = null
-
-    private var vidCapture: VideoCapture<Recorder>? = null
-
     private var recCapture: Recording? = null
+
+    private var imageCapture: ImageCapture? = null
+
+    private var videoCapture: VideoCapture<Recorder>? = null
 
     private lateinit var cameraXExecutor: ExecutorService
 
@@ -111,9 +126,11 @@ class ViewCameraX : Fragment() {
         }
         bind.cameraxImageCaptureButton.setOnClickListener {
             Log.d("$javaClass","cameraxImageCaptureButton.setOnClickListener")
+            this.takePhoto()
         }
         bind.cameraxVideoCaptureButton.setOnClickListener {
             Log.d("$javaClass","cameraxVideoCaptureButton.setOnClickListener")
+            this.captureVideo()
         }
     }
 
@@ -163,11 +180,140 @@ class ViewCameraX : Fragment() {
         Log.i(LOG_INFO_TAG, FragLcTags.LOG_DETACH)
     }
 
-    private fun takePhoto() {}
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext().applicationContext)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also { it.setSurfaceProvider(bind.cameraxPreviewView.surfaceProvider) }
+            val recorder = Recorder.Builder()
+                .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
+                .build()
+            videoCapture = VideoCapture.withOutput(recorder)
+            imageCapture = ImageCapture.Builder().build()
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                    .also {
+                        it.setAnalyzer(cameraXExecutor, LuminosityAnalyzer { luma ->
+                            Log.d(TAG, "Average luminosity: $luma") })
+                    }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this
+                    , cameraSelector
+                    , preview
+                    , imageCapture
+//                    , imageAnalyzer
+//                    , videoCapture
+                )
+            } catch(e: Exception) {
+                Log.e(TAG, "Use case binding failed: $e")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
 
-    private fun captureVideo() {}
+    private fun takePhoto() {
+        val imgCapture = this.imageCapture ?: return
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                    .format(System.currentTimeMillis())
+        val contentValues = ContentValues()
+                .apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+                        }
+                }
+        val outputOptions = ImageCapture.OutputFileOptions
+                                .Builder(requireContext().contentResolver,
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                                        .build()
 
-    private fun startCamera() {}
+        imgCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(e: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${e.message}", e)
+                }
+                override fun onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
+
+    private fun captureVideo() {
+        val vidCapture = this.videoCapture ?: return
+
+        bind.cameraxVideoCaptureButton.isEnabled = false
+
+        if (recCapture != null) {
+            recCapture!!.stop()
+            recCapture = null
+            return
+        }
+        
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
+            }
+        }
+
+        val mediaStoreOutputOptions = MediaStoreOutputOptions
+            .Builder(requireContext().contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+        recCapture = vidCapture.output
+            .prepareRecording(requireContext(), mediaStoreOutputOptions)
+            .apply {
+                if (PermissionChecker.checkSelfPermission(requireActivity(),
+                        Manifest.permission.RECORD_AUDIO) ==
+                    PermissionChecker.PERMISSION_GRANTED)
+                {
+                    withAudioEnabled()
+                }
+            }
+            .start(ContextCompat.getMainExecutor(requireContext())) { recordEvent ->
+                when(recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        bind.cameraxVideoCaptureButton.apply {
+                            text = getString(pkg.what.pq.R.string.camerax_stop_capture)
+                            isEnabled = true
+                        }
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        if (!recordEvent.hasError()) {
+                            val msg = "Video capture succeeded: " +
+                                    "${recordEvent.outputResults.outputUri}"
+                            Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT)
+                                .show()
+                            Log.d(TAG, msg)
+                        } else {
+                            recCapture?.close()
+                            recCapture = null
+                            Log.e(TAG, "Video capture ends with error: " +
+                                    "${recordEvent.error}")
+                        }
+                        bind.cameraxVideoCaptureButton.apply {
+                            text = getString(pkg.what.pq.R.string.camerax_start_capture)
+                            isEnabled = true
+                        }
+                    }
+                }
+            }
+
+
+    }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
@@ -189,4 +335,22 @@ class ViewCameraX : Fragment() {
                 }
             }.toTypedArray()
     }
+
+    private inner class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()
+                val data = ByteArray(remaining())
+                    get(data)
+            return data
+        }
+        override fun analyze(image: ImageProxy) {
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+            listener(luma)
+            image.close()
+        }
+    }
+
 }
